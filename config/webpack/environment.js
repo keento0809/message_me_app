f005a651a00eb43387cff3bd052da630a7c620f9
keento09 <biglobe0210@gmail.com>
2021-02-06 12:39:07 +0900

    Add favicon etc

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
+// 追記：ここから
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+// 追記：ここまで
+
+
 module.exports = environment
+
+
