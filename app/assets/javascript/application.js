4d9610920197f2b67447e429ca310e7c35339370
keento09 <biglobe0210@gmail.com>
2021-02-07 17:18:07 +0900

    Forgot to commit for a while..

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -2,6 +2,9 @@
 //= require semantic-ui
 
 // ドロップダウンを使用するためのコード
-$(function(){
-    $('.ui.dropdown').dropdown();
-  })
+$(function () {
+  $(".ui.dropdown").dropdown();
+  $(".message .close").on("click", function () {
+    $(this).closest(".message").transition("fade");
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'chatroom/index'
   get 'sessions/new'
   root 'chatroom#index'
+  post 'login', to: 'sessions#create'
   get 'login', to: 'sessions#new'
+  delete 'logout', to: 'sessions#destroy'
 end
